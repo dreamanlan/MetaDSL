871ddb12d35d018e880113d5d0c0846be5708ae0
dreamanlan <dreamanlan@tencent.com>
2022-05-10 21:49:48 +0800

    测试代码换成简单代码

--- bin/Debug/copy.h
+++ bin/Debug/copy.h
@@ -1,44 +1,17 @@
 @@delimiter(script, "={:=", "=:}=");
-namespace cv
+[[13]]int main()
 {
-	class CV_EXPORTS MinProblemSolver ":" public Algorithm
+	struct index_st
 	{
-		public ":";
-		class CV_EXPORTS Function
-		{
-			public ":";
-			virtual "~" Function()
-			{
-			};
-			virtual int getDims()const "=" 0;
-			virtual double getGradientEps()const;
-			virtual double calc(const double "*" x)const "=" 0;
-			virtual void getGradient(const double "*" x, double "*" grad);
-		};
-		virtual Ptr "<" Function ">" getFunction()const "=" 0;
-		virtual void setFunction(const Ptr "<" Function ">" "&" f)"=" 0;
-		virtual TermCriteria getTermCriteria()const "=" 0;
-		virtual void setTermCriteria(const TermCriteria "&" termcrit)"=" 0;
-		virtual double minimize(InputOutputArray x)"=" 0;
+		unsigned char type;
+		unsigned char num;
+		unsigned int len;
 	};
-	class CV_EXPORTS DownhillSolver ":" public MinProblemSolver
-	{
-		public ":";
-		virtual void getInitStep(OutputArray step)const "=" 0;
-		virtual void setInitStep(InputArray step)"=" 0;
-		static Ptr "<" DownhillSolver ">" create(const Ptr "<" MinProblemSolver "::" Function ">" "&" f "=" Ptr "<" MinProblemSolver "::" Function ">"(), InputArray initStep "=" Mat_ "<" double ">"(1, 1, 0.0), TermCriteria termcrit "=" TermCriteria(TermCriteria "::" MAX_ITER "+" TermCriteria "::" EPS, 5000, 0.000001));
-	};
-	class CV_EXPORTS ConjGradSolver ":" public MinProblemSolver
-	{
-		public ":";
-		static Ptr "<" ConjGradSolver ">" create(const Ptr "<" MinProblemSolver "::" Function ">" "&" f "=" Ptr "<" ConjGradSolver "::" Function ">"(), TermCriteria termcrit "=" TermCriteria(TermCriteria "::" MAX_ITER "+" TermCriteria "::" EPS, 5000, 0.000001));
-	};
-	enum SolveLPResult
-	{
-		SOLVELP_UNBOUNDED "=" "-" 2;
-		SOLVELP_UNFEASIBLE "=" "-" 1;
-		SOLVELP_SINGLE "=" 0;
-		SOLVELP_MULTI "=" 1;
-	};
-	CV_EXPORTS_W int solveLP(const Mat "&" Func, const Mat "&" Constr, Mat "&" z);
+	typedef struct index_st index_t;
+	int x;
+	y;
+	x "=" offsetof(struct index_st, len);
+	y "=" offsetof(index_t, num);
+	return 0;
 };
+abc(()abcd);
