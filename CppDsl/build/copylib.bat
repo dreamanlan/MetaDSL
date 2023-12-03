d21a2e3909d27716db21b287ac152e3c154c8c6f
dreamanlan <dreamanlan@tencent.com>
2023-12-03 23:29:42 +0800

    Update copylib.bat

--- CppDsl/build/copylib.bat
+++ CppDsl/build/copylib.bat
@@ -8,3 +8,6 @@ xcopy /Y/S Plugins\* ..\lib\*
 xcopy /Y/S ..\lib\* ..\..\..\..\UGit\ScriptableUnreal\Plugins\Brace\Source\Brace\lib\*
 xcopy /Y/S ..\lib\* ..\..\..\yuzu\externals\brace\lib\*
 
+xcopy /Y/S ..\include\* ..\..\..\..\UGit\ScriptableUnreal\Plugins\Brace\Source\Brace\Private\include\*
+xcopy /Y/S ..\include\* ..\..\..\yuzu\externals\brace\include\*
+
