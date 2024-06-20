36dc67298fa1dbed32cdc6bf89495b185f81d9c2
dreamanlan <dreamanlan@tencent.com>
2024-06-20 13:01:42 +0800

    ios & osx build sync

--- CppDsl/build/copylib.bat
+++ CppDsl/build/copylib.bat
@@ -6,8 +6,8 @@ cd %workdir%
 xcopy /Y/S Plugins\* ..\lib\*
 
 xcopy /Y/S ..\lib\* ..\..\..\..\UGit\ScriptableUnreal\Plugins\Brace\Source\Brace\lib\*
-xcopy /Y/S ..\lib\* ..\..\..\yuzu\externals\brace\lib\*
+xcopy /Y/S ..\lib\* ..\..\..\suyu\externals\brace\lib\*
 
 xcopy /Y/S ..\include\* ..\..\..\..\UGit\ScriptableUnreal\Plugins\Brace\Source\Brace\Private\include\*
-xcopy /Y/S ..\include\* ..\..\..\yuzu\externals\brace\include\*
+xcopy /Y/S ..\include\* ..\..\..\suyu\externals\brace\include\*
 
