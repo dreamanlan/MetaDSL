703db836efa526b54472d6aa74afb60dbc92137c
dreamanlan <dreaman_163@163.com>
2025-05-16 03:38:45 +0800

    rebuild

--- CppDsl/build/copylib.bat
+++ CppDsl/build/copylib.bat
@@ -6,8 +6,8 @@ cd %workdir%
 xcopy /Y/S Plugins\* ..\lib\*
 
 xcopy /Y/S ..\lib\* ..\..\..\..\UGit\ScriptableUnreal\Plugins\Brace\Source\Brace\lib\*
-xcopy /Y/S ..\lib\* ..\..\..\suyu\externals\brace\lib\*
+xcopy /Y/S ..\lib\* ..\..\..\myuzu\externals\brace\lib\*
 
 xcopy /Y/S ..\include\* ..\..\..\..\UGit\ScriptableUnreal\Plugins\Brace\Source\Brace\Private\include\*
-xcopy /Y/S ..\include\* ..\..\..\suyu\externals\brace\include\*
+xcopy /Y/S ..\include\* ..\..\..\myuzu\externals\brace\include\*
 
